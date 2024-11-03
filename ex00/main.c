5d4172e411a785edeafbdf70ac37dc764bdfedef
Francesc Banzo Saavedra <fbanzo-s@cbr13s5.42barcelona.com>
2024-11-03 21:34:14 +0100

    rush02

--- ex00/main.c
+++ ex00/main.c
@@ -6,47 +6,45 @@
 /*   By: fbanzo-s <fbanzo-s@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 16:29:46 by fbanzo-s          #+#    #+#             */
-/*   Updated: 2024/11/03 15:35:18 by fbanzo-s         ###   ########.fr       */
+/*   Updated: 2024/11/03 21:33:01 by fbanzo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#define BUFFER_SIZE 1024
 
 int		ft_strlen(char *str);
-
-char	*ft_strstr(char *str, char *to_find);
 
 int		ft_isdigit(char c);
 
 int		ft_atoi(char *str);
 
+char	*ft_strstr(char *str, char *to_find);
+
 char	*ft_strcu(char *str, char to_find);
 
-char	*ft_strc(char *str, char to_find)
-{
-	char	*linestart;
-	int		i;
+void	ft_putstr(char *str);
 
-	linestart = str;
-	while (*linestart)
-	{
-		if (ft_isdigit(*linestart) && ft_isdigit(*(linestart + 1)))
-		{
-			i = 0;
-			while (linestart[i] == to_find)
-			{
-				i++;
-			}
-			if (i > 0)
-				return (linestart);
-		}
-		linestart++;
-	}
-	return (NULL);
-}
+void	ft_putchar(char c);
+
+char	*ft_strc(char *str, char to_find);
+
+char	*ft_skipchar(char *str, char c);
+
+void	checkdec(char *num);
+
+void	checkcent(char *numstr);
+
+void	readfile(char *num, int numlen);
+
+void	readfiledec(char *num, int numlen);
+
+void	readfilec(char numc, int numlen);
+
+void	rmvfirstn(char *num, char *tmp, int j);
+
+void	checkthousand(char *numstr, char *tmp);
 
 char	*getdesc(char *match, int numlen)
 {
@@ -55,18 +53,9 @@ char	*getdesc(char *match, int numlen)
 	char	*str;
 
 	desc = match + numlen;
-	while (*desc && *desc == ' ')
-	{
-		desc++;
-	}
-	while (*desc && *desc == ':')
-	{
-		desc++;
-	}
-	while (*desc && (*desc == ' '))
-	{
-		desc++;
-	}
+	desc = ft_skipchar(desc, ' ');
+	desc = ft_skipchar(desc, ':');
+	desc = ft_skipchar(desc, ' ');
 	i = 0;
 	while (desc[i] != '\n')
 	{
@@ -76,66 +65,23 @@ char	*getdesc(char *match, int numlen)
 	return (desc);
 }
 
-void	readfile(char *num, int numlen)
+void	manejarnum(char *numstr, int numlen, int numint)
 {
-	int		fd;
-	ssize_t	bytesread;
-	char	*buffer;
-	char	*match;
-	char	*desc;
+	char	tmp[40];
+	int		i;
 
-	fd = open("numbers.dict", O_RDONLY);
-	buffer = (char *)malloc(BUFFER_SIZE);
-	bytesread = read(fd, buffer, BUFFER_SIZE - 1);
-	while (bytesread > 0)
+	if (numlen <= 2)
 	{
-		buffer[bytesread] = '\0';
-		match = ft_strstr(buffer, num);
-		if (match != NULL)
-		{
-			desc = getdesc(match, numlen);
-			write(1, desc, ft_strlen(desc));
-		}
-		bytesread = read(fd, buffer, BUFFER_SIZE - 1);
+		checkdec(numstr);
 	}
-	free(buffer);
-	close(fd);
-}
-
-void	readfiledec(char *num, int numlen)
-{
-	int		fd;
-	ssize_t	bytesread;
-	char	*buffer;
-	char	*match;
-	char	*desc;
-
-	fd = open("numbers.dict", O_RDONLY);
-	buffer = (char *)malloc(BUFFER_SIZE);
-	bytesread = read(fd, buffer, BUFFER_SIZE - 1);
-	while (bytesread > 0)
+	else if (numlen == 3)
 	{
-		buffer[bytesread] = '\0';
-		match = ft_strc(buffer, num[0]);
-		if (match != NULL)
-		{
-			desc = getdesc(match, numlen);
-			write(1, desc, ft_strlen(desc));
-		}
-		if (num[1] != '0')
-		{
-			match = ft_strcu(buffer, num[1]);
-			write(1, " ", 1);
-			if (match != NULL)
-			{
-				desc = getdesc(match, numlen);
-				write(1, desc, ft_strlen(desc));
-			}
-		}
-		bytesread = read(fd, buffer, BUFFER_SIZE - 1);
+		checkcent(numstr);
 	}
-	free(buffer);
-	close(fd);
+	else if (numlen == 4)
+	{
+		checkthousand(numstr, tmp);
+	}
 }
 
 int	main(int ac, char **av)
@@ -149,22 +95,10 @@ int	main(int ac, char **av)
 		numstr = av[1];
 		numint = ft_atoi(numstr);
 		numlen = ft_strlen(numstr);
-		if (numlen < 3)
-		{
-			if (numint <= 20 && numint >= 0)
-			{
-				readfile(numstr, numlen);
-			}
-			else
-			{
-				readfiledec(numstr, numlen);
-			}
-		}
+		manejarnum(numstr, numlen, numint);
 	}
 	else if (ac == 3)
-	{
 		return (0);
-	}
 	else
 		return (0);
 }
